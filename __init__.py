97191abad687822a8354e5ae2312f3b42a7f327e
bmeares <bennett.meares@gmail.com>
2021-05-19 18:00:44 +0000

    SSO login is working for Google

--- __init__.py
+++ __init__.py
@@ -6,28 +6,54 @@
 Meerschaum plugin to add SSO login functionality to the API.
 """
 
+import json
 from meerschaum.api import app
-from .config import get_sso_config
-from fastapi_sso.sso.google import GoogleSSO
+from fastapi import FastAPI
+from starlette.config import Config
 from starlette.requests import Request
-client_id = '60263884711-6a9e84d0eii8epp9mokc7lfv6a0kpmnd.apps.googleusercontent.com'
-client_secret = 'fUTLiqLkAmfq0oi8nmyTvryz'
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import HTMLResponse, RedirectResponse
+from authlib.integrations.starlette_client import OAuth, OAuthError
+from .config import get_sso_config
 
+prepend_path = get_sso_config('prepend')
 google_client_id = get_sso_config('google', 'id')
 google_client_secret = get_sso_config('google', 'secret')
-google_sso = GoogleSSO(google_client_id, google_client_secret, 'http://localhost:8000/sso/google/callback')
+google_callback_url = get_sso_config('google', 'callback')
 
-required = ['fastapi-sso']
+GOOGLE_CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth = OAuth()
+oauth.register(
+    name='google',
+    client_id=google_client_id,
+    client_secret=google_client_secret,
+    server_metadata_url=GOOGLE_CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile',
+    },
+)
+app.add_middleware(SessionMiddleware, secret_key=google_client_secret)
 
-@app.get('/sso/google')
-async def test_sso():
-    return await google_sso.get_login_redirect()
+required = ['authlib', 'httpx']
+prepend_path = '/api'
+sso_google_path = prepend_path + '/sso/google'
 
-@app.get('/sso/google/callback')
+@app.get(sso_google_path)
+async def login_sso(request: Request):
+    #  redirect_uri = sso_google_path + '/callback'
+    redirect_uri = google_callback_url
+    print(redirect_uri)
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+    #  return await google_sso.get_login_redirect()
+
+@app.get(sso_google_path + '/callback')
 async def google_callback(request: Request):
-    print(request.cookies)
-    user = await google_sso.verify_and_process(request)
-    return {'message' : 'success!'}
+    #  print(request.cookies)
+    #  user = await google_sso.verify_and_process(request)
+    token = await oauth.google.authorize_access_token(request)
+    user = await oauth.google.parse_id_token(request, token)
+    return user
+    #  return {'message' : 'success!'}
     #  return {
         #  "id": user.id,
         #  "picture": user.picture,
--- config.py
+++ config.py
@@ -9,9 +9,11 @@ from meerschaum.config import get_plugin_config, write_plugin_config
 def get_sso_config(*args, **kw):
     _cf = get_plugin_config(*args, warn=False, **{k:v for k in kw if k != 'warn'})
     if not _cf:
+        _prepend = prompt('Prepend path to URL (blank for the root to be /sso):')
         _id = prompt('Google Client ID:')
         _secret = prompt('Google Client Secret:', is_password=True)
-        _cf = {'google' : {'id' : _id, 'secret' : _secret}}
+        _callback = prompt('Google Callback URL:')
+        _cf = {'prepend' : _prepend, 'google' : {'id' : _id, 'secret' : _secret, 'callback' : _callback}}
         write_sso_config(_cf)
     return get_plugin_config(*args, **kw)
 
