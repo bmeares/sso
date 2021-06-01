fec1122cb6ca995963f9379aa38dc200f507b1e2
Aaron Jordan <aaron@impactfulmedia.co>
2021-06-01 00:44:04 +0000

    changes to init

--- __init__.py
+++ __init__.py
@@ -13,7 +13,9 @@ from meerschaum.plugins import api_plugin
 @api_plugin
 def init(app):
     import json
-    from fastapi import FastAPI
+    from urllib.parse import quote
+    from typing import Optional
+    from fastapi import FastAPI, Response, Cookie, Header
     from starlette.config import Config
     from starlette.requests import Request
     from starlette.middleware.sessions import SessionMiddleware
@@ -22,10 +24,13 @@ def init(app):
     from .config import get_sso_config, GOOGLE_CONF_URL
     from meerschaum import get_connector
 
+#    staging_port = get_sso_config('staging_port')
+
     prepend_path = get_sso_config('prepend')
     google_client_id = get_sso_config('google', 'id')
     google_client_secret = get_sso_config('google', 'secret')
-    google_callback_url = get_sso_config('google', 'callback')
+    google_callback_slug = get_sso_config('google', 'callback')
+#    google_callback_url_s = get_sso_config('google', 'callback_s')
 
     oauth = OAuth()
     oauth.register(
@@ -40,10 +45,16 @@ def init(app):
     app.add_middleware(SessionMiddleware, secret_key=google_client_secret)
 
     sso_google_path = prepend_path + '/sso/google'
+    redirect_target = '/'
 
     @app.get(sso_google_path)
-    async def login_sso(request: Request):
-        redirect_uri = google_callback_url
+    async def login_sso(request: Request, referer: Optional[str] = Header(None)):
+        redirect_full_uri = ':'.join(str(referer).split(':')[0:2]) 
+        redirect_base_uri = '/'.join(redirect_full_uri.split('/')[0:3])
+        redirect_uri = redirect_base_uri + google_callback_slug
+
+        print(redirect_base_uri + ' -> ' + redirect_uri)
+
         return await oauth.google.authorize_redirect(request, redirect_uri)
 
     @app.get(sso_google_path + '/callback')
@@ -53,11 +64,11 @@ def init(app):
 
         conn = get_connector('sql', 'wedding_s')
         q = f"SELECT address FROM \"people-with-contact-info\" WHERE email = '{user.email}'"
-        address = conn.value(q)
+        address = quote(conn.value(q))
 
-        response = RedirectResponse('/login_success')
-        response.set_cookie(key='login_name', value=user.name)
-        response.set_cookie(key='login_email', value=user.email)
+        response = RedirectResponse(redirect_target)
+        response.set_cookie(key='login_name', value=quote(user.name))
+        response.set_cookie(key='login_email', value=quote(user.email))
         response.set_cookie(key='login_address', value=address)
         return response
 
